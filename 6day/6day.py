2c977b3c24c0dd3cba924578e573d20fcfda755b
PPPP <PPPP@PPP.COM>
2021-01-26 11:55:50 +0800

    [新增作业]：第6天作业，统计目录下文章中你认为重要的单词

--- 6day/6day.py
+++ 6day/6day.py
@@ -1,19 +1,25 @@
 #coding:utf-8
 import re
-wb_path = "./wb.txt"
+import glob
+wb_path = "file/*.txt"
+wb_path_all  = glob.glob(wb_path)
 zy = ["We","billing","a","are"]
-f = open(wb_path)
-a = 0
-for line2 in f:
-    pass
-list = line2.split(" ")#将读取到的文件内容以空格分隔然后转为list
-new_list = []
-for i in list:
-    matchObj = re.match(r'[a-zA-Z]*', i,  re.M)
-    new_list.append(matchObj.group())
 
-for i in new_list:
-    if i in zy:
-        a = a+1
-        print(i)
-print("此文章重要的单词出现的次数为 {} 次".format(a))
+a = 0
+ps = 0
+for path in wb_path_all:
+    ps = ps + 1
+    f = open(path)
+    for line2 in f:
+        pass
+    list = line2.split(" ")#将读取到的文件内容以空格分隔然后转为list
+    new_list = []
+    for i in list:
+        matchObj = re.match(r'[a-zA-Z]*', i,  re.M)
+        new_list.append(matchObj.group())
+    print("new_list{}".format(new_list))
+    for i in new_list:
+        if i in zy:
+            a = a+1
+            # print(i)
+print("{}篇文章重要的单词出现的次数总共为 {} 次".format(ps,a))
--- 6day/demo.py
+++ 6day/demo.py
@@ -2,5 +2,24 @@ import re
 
 line = "settings7"
 
-matchObj = re.match(r'[a-zA-Z]*', line,  re.M)
-print(matchObj.group())
+matchObj = re.match(r'[a-zA-Z]*', line,  re.M)#只提取字符串中的字母
+line1 = matchObj.group()#将提取的字符串赋值给变量 line1
+
+# re模块的使用
+"""
+re.search(pattern, string, flags=0)
+正则匹配
+#各参数含协议
+pattern:匹配的正则表达式
+string:要匹配的字符串
+flags:标志位，用于控制正则表达式的匹配方式，如：是否区分大小写，多行匹配等等
+    flags : 可选，表示匹配模式，比如忽略大小写，多行模式等，具体参数为：
+    re.I 忽略大小写
+    re.L 表示特殊字符集 \w, \W, \b, \B, \s, \S 依赖于当前环境
+    re.M 多行模式
+    re.S 即为 . 并且包括换行符在内的任意字符（. 不包括换行符）
+    re.U 表示特殊字符集 \w, \W, \b, \B, \d, \D, \s, \S 依赖于 Unicode 字符属性数据库
+    re.X 为了增加可读性，忽略空格和 # 后面的注释
+
+
+"""
